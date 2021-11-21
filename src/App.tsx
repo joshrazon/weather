4eea9dd599f26b67bc20fa1b248ebfbb43e7da5f
joshrazon <joshrazonn@gmail.com>
2021-11-20 21:27:38 -0700

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Button from '@material-ui/core/Button';
+
+export default function App() {
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </div>
+  );
+}
+
+/**
+ * name
+ * sys.country
+ * main
+ */
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-react', { runtime: 'automatic' }]],
+          },
+        },
+      },
+      {
+        test: /\.html$/,
+        use: 'html-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: './dist',
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'React App',
+      template: 'src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
