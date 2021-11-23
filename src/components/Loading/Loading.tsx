ba98ed49181ae29dd6a2f99f6b6b5a4453fe2ead
joshrazon <joshrazonn@gmail.com>
2021-11-22 21:17:46 -0700

    created loading component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,12 @@
+import LoopIcon from '@material-ui/icons/Loop';
+import './Loading.css';
+
+function Loading() {
+  return (
+    <div className="loading-container">
+      <LoopIcon id="loading" />
+    </div>
+  );
+}
+
+export default Loading;
