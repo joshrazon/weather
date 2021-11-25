71c7f4d965e2436fdecf2f7ef48caf1bfa71d004
joshrazon <joshrazonn@gmail.com>
2021-11-25 09:23:37 -0700

    handle no city found error

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,4 @@
-import { Button, Container, makeStyles, Card } from '@material-ui/core';
+import { Button, Container, Box, Card, Typography } from '@material-ui/core';
 import weatherData from '../components/WeatherPanel/weatherData';
 import WeatherPanel from '../components/WeatherPanel/WeatherPanel';
 import { UserInputs } from '../components/UserInputs';
@@ -14,19 +14,18 @@ export default function App() {
   const [weather, setWeather] = useState<Weather>({} as Weather);
   const [location, setLocation] = useState<Location>({} as Location);
   const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
   const classes = useStyles();
 
   const requestWeather = async (city: string, countryCode: string) => {
     setLoading(true);
+
     let response = await fetchWeather(city, countryCode);
     let data = await response.json();
+    console.log('response from fetch request', data);
+    handleWeatherResponse(data);
 
-    if (data.main) {
-      console.log('response from fetch request', data);
-      return handleWeatherResponse(data);
-    } else {
-      setLoading(false);
-    }
+    return setLoading(false);
   };
 
   const handleWeatherResponse = (data: any) => {
@@ -35,12 +34,19 @@ export default function App() {
       setLocation(newLocation);
       setWeather(data.main);
     } else {
+      // HANDLE ERROR HERE
       console.log(data.message);
+      setError(true);
     }
+
+    if (error) {
+      setError(false);
+    }
+
     return setLoading(false);
   };
 
-  const handleLoading = () => {
+  const toggleLoading = () => {
     setLoading(!loading);
   };
 
@@ -50,14 +56,19 @@ export default function App() {
         <Container maxWidth="sm" className={classes.container}>
           <Card raised={true} className={classes.cardLayout}>
             <UserInputs requestWeather={requestWeather} />
+            {error && (
+              <Container maxWidth="sm" className={classes.cardLayout}>
+                <Typography className={classes.error} component="span" variant="body2">
+                  City not found
+                </Typography>
+              </Container>
+            )}
 
             {loading ? (
               <Loading />
             ) : (
               <WeatherPanel weather={weather} location={location} />
             )}
-
-            <Button onClick={handleLoading}>Toggle Loading</Button>
           </Card>
         </Container>
       </main>
